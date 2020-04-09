96a270cbbb46670fa507a1bdae39cc149ab407cf
VladislavNep <VladislavNep@yandex.ru>
2020-04-09 22:39:39 +0700

    rename directory

--- search_movies/urls.py
+++ search_movies/urls.py
@@ -1,4 +1,4 @@
-"""django_movie_site URL Configuration
+"""search_movies URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- search_movies/wsgi.py
+++ search_movies/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_movie_site project.
+WSGI config for search_movies project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_movie_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'search_movies.settings')
 
 application = get_wsgi_application()
