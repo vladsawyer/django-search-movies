0f5fd8525170312889264a0a43f72c3f7555d285
VladislavNep <VladislavNep@yandex.ru>
2020-09-07 19:17:27 +0700

    fix

--- movies/forms.py
+++ movies/forms.py
@@ -1,14 +1,8 @@
 from django import forms
-from movies.models import Comments, Likes
+from movies.models import Comments
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comments
         fields = ("text", "image", "user")
-
-
-class LikeForm(forms.ModelForm):
-    class Meta:
-        model = Likes
-        fields = ("user", "value")
