85e2b701bbf99ac9d187ec1971d6af2fa2928497
VladislavNep <VladislavNep@yandex.ru>
2020-10-01 21:09:50 +0700

    fix

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from django.views.generic.base import View
+import logging
 
 
 JSON_DUMPS_PARAMS = {
     'ensure_ascii': False
 }
+logger = logging.getLogger(__name__)
 
 
 class BaseView(View):
@@ -16,6 +18,7 @@ class BaseView(View):
         try:
             response = super().dispatch(request, *args, **kwargs)
         except Exception as e:
+            logger.warning(e)
             return self._response({'errorMessage': str(e)}, status=400)
 
         if isinstance(response, (dict, list)):
